89d60077827f2a3dbd94dc6049511ca2003f9452
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2024-05-14 14:51:21 -0300

    feat: estrutura inicial para um sistema básico

---
+++ simpleSystem/src/main/java/com/example/simpleSystem/SimpleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSystemApplication.class, args);
+	}
+
+}
