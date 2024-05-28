c9bbd02326e0553267f290946dff57bbcf5a2987
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2024-05-28 12:30:28 -0300

    feat: adicionando serialização de yaml

--- simpleSystem/src/main/java/com/example/simpleSystem/config/WebConfig.java
+++ simpleSystem/src/main/java/com/example/simpleSystem/config/WebConfig.java
@@ -1,12 +1,20 @@
 package com.example.simpleSystem.config;
 
+import com.example.simpleSystem.serializationconverter.YamlJacksonToHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.example.simpleSystem.util.MediaType;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.add(new YamlJacksonToHttpMessageConverter());
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -24,9 +32,10 @@ public class WebConfig implements WebMvcConfigurer {
         configurer.favorParameter(false)
                 .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
+                .defaultContentType(org.springframework.http.MediaType.valueOf(MediaType.APPLICATION_JSON))
+                .mediaType("json", org.springframework.http.MediaType.valueOf(MediaType.APPLICATION_JSON))
+                .mediaType("xml", org.springframework.http.MediaType.valueOf(MediaType.APPLICATION_XML))
+                .mediaType("x-yml", org.springframework.http.MediaType.valueOf(MediaType.APPLICATION_YML));
 
     }
 
--- simpleSystem/src/main/java/com/example/simpleSystem/controllers/PersonController.java
+++ simpleSystem/src/main/java/com/example/simpleSystem/controllers/PersonController.java
@@ -2,12 +2,12 @@ package com.example.simpleSystem.controllers;
 
 import com.example.simpleSystem.domain.Person;
 import com.example.simpleSystem.services.PersonServices;
+import com.example.simpleSystem.util.MediaType;
 import java.util.List;
 import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,23 +19,23 @@ public class PersonController {
     @Autowired
     private PersonServices personServices;
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public List<Person> findAllPeople(){
         return personServices.repository.findAll();
     }
 
-    @GetMapping(value = "{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(value = "{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public Optional<Person> findPersonById(@PathVariable Long id){
          return personServices.repository.findById(id);
     }
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     private ResponseEntity createPerson(@RequestBody Person person){
         personServices.repository.save(person);
         return ResponseEntity.ok("Person created");
     }
 
-    @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     private ResponseEntity updatePerson(@RequestBody Person person){
         Optional<Person> optionalPerson = personServices.repository.findById(person.getId());
         if(optionalPerson.isPresent()){
